21796e011634d11b847789b849343ac412cd9ac5
ShyamPandit3107 <shyambpandit3107@gmail.com>
2024-08-09 17:19:52 +0530

    fixed building issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,46 @@
 "use client";
-import { useStore } from "@/store";
-import { useWebsiteInfoByInstitute } from "@/api/api-hooks";
-import CarouselComponent from "@/components/home/carousel-component";
 import AboutIns from "@/components/home/about-ins";
 import FounderMessage from "@/components/home/founder-message";
-import Event from "@/components/home/notices";
-import Page from "./(main)/page";
+import Notices from "@/components/home/notices";
+import Events from "@/components/home/events";
+import CarouselComponent from "@/components/home/carousel-component";
+import { useStore } from "@/store";
+import { useWebsiteInfoByInstitute } from "@/api/api-hooks";
 
 export default function Home() {
-  return <Page></Page>;
+  const id = useStore((state) => state.id);
+  const { data: websiteInfoByInstitute } = useWebsiteInfoByInstitute(id);
+  return (
+    <div className="w-full">
+      {websiteInfoByInstitute?.one_ins?.landing_control?.home_background_object
+        ?.images.length > 0 && (
+        <CarouselComponent
+          images={
+            websiteInfoByInstitute?.one_ins?.landing_control
+              ?.home_background_object?.images
+          }
+        />
+      )}
+      {websiteInfoByInstitute?.one_ins?.landing_control
+        ?.home_about_institute_object && (
+        <AboutIns
+          home_object={
+            websiteInfoByInstitute?.one_ins?.landing_control
+              ?.home_about_institute_object
+          }
+        />
+      )}
+      {websiteInfoByInstitute?.one_ins?.landing_control
+        ?.about_society_dynamic && (
+        <FounderMessage
+          data={
+            websiteInfoByInstitute?.one_ins?.landing_control
+              ?.about_society_dynamic
+          }
+        />
+      )}
+      <Notices />
+      <Events />
+    </div>
+  );
 }
